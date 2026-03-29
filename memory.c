44c81f208fa640b14fdb8d6f7d8864e206212294
Alexandros Grammatikopoulos <alexandrosgramma@gmail.com>
2026-03-29 19:31:42 +0300

    Add calls and functions.

--- memory.c
+++ memory.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-
+#include <stdio.h>
 #include "memory.h"
 #include "vm.h"
 
@@ -18,6 +18,15 @@ void* reallocate(void* pointer, size_t oldSize, size_t newSize)
 
 static void freeObject(Obj* object) {
 	switch(object->type) {
+		case OBJ_FUNCTION: {
+			ObjFunction* function = (ObjFunction*)object;
+			freeChunk(&function->chunk);
+			FREE(ObjFunction, object);
+			break;
+		}
+		case OBJ_NATIVE:
+			printf("<native fn>");
+			break;
 		case OBJ_STRING: {
 			ObjString* string = (ObjString*)object;
 			FREE_ARRAY(char, string->chars, string->length + 1);
